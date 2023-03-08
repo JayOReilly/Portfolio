06bd2dfdf361f0ad476105aeb0abf9137a662805
JayOReilly <99806112+JayOReilly@users.noreply.github.com>
2023-03-08 20:52:36 +0000

    new edit4

--- next.config.js
+++ next.config.js
@@ -3,14 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ]
-  },
-}
+module.exports = nextConfig
